62a58d667448327f79e2bd24d91ef4c2eecfc49f
Mandy Tong <mandytong@CroboMBA3s-MacBook-Pro.local>
2017-10-19 11:43:18 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,19 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+console.log("User flipped queen");
+console.log("User flipped king");
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = [0];
+cardInPlay.push(cardOne);
+console.log("User flipped queen");
+var cardTwo = [2];
+cardInPlay.push(cardTwo);
+
+if (cardInPlay.length === 2) {
+	(cardsInPlay [0] === cardsInPlay [1])
+    alert("You found a match!");
+ } 
+ else {
+ 	alert("Sorry, try again.");
+}
+
